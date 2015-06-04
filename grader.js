940fd5fead4e97bb8b79a178011e474f8a3514fd
Vinh Nguyen <vnguyen.cheme@gmail.com>
2015-06-04 15:47:28 -0600

    linted the grader.js

--- grader.js
+++ grader.js
@@ -1,65 +1,66 @@
+/*jshint funcscope:true*/
 module.exports = {
   letterGrader: function () {
-  var letterGrade = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F+", "F"]
-  var numbGrade = [98, 92, 90, 88, 82, 80, 78, 72, 70, 68, 62, 60, 58, 0]
+  var letterGrade = ["A+", "A", "A-", "B+", "B", "B-", "C+", "C", "C-", "D+", "D", "D-", "F+", "F"];
+  var numbGrade = [98, 92, 90, 88, 82, 80, 78, 72, 70, 68, 62, 60, 58, 0];
   var results = [];
   if (arguments.length === 1) {
     for (var i = 0; i < numbGrade.length; i ++) {
       if (arguments[0] >= numbGrade[i]) {
-        results = i
-        i = numbGrade.length
+        results = i;
+        i = numbGrade.length;
       }
-    };
-    return letterGrade[results]
+    }
+    return letterGrade[results];
   } else {
-    for (var j = 0; j < arguments.length; j ++ ) {
-      for (var i = 0; i < numbGrade.length; i ++) {
-        if (arguments[j] >= numbGrade[i]) {
-          results[j] = letterGrade[i]
-          i = numbGrade.length
+    for (i = 0; i < arguments.length; i ++ ) {
+      for (var j = 0; j < numbGrade.length; j ++) {
+        if (arguments[i] >= numbGrade[j]) {
+          results[i] = letterGrade[j];
+          j = numbGrade.length;
         }
-      };
-    };
-    return results
+      }
+    }
+    return results;
   }
   },
 
   averageScore: function (array) {
     var total = array.reduce(function (all, item, index, array) {
-      all += item
-      return all
+      all += item;
+      return all;
     }, 0);
-    return total/array.length
+    return total/array.length;
   },
 
   medianScore: function (array) {
     var length = array.length;
-    return length % 2 === 0 ? (array[length/2 - 1] + array[(length/2)])/2 : array[Math.floor(length/2)]
+    return length % 2 === 0 ? (array[length/2 - 1] + array[(length/2)])/2 : array[Math.floor(length/2)];
   },
 
   modeScore: function (array) {
     var modeCompare = 0;
     var modeResult = [];
     var obj = array.reduce(function (all, item, index) {
-    return all[item] === undefined ? (all[item] = 1 , all) : (all[item] += 1 , all)
+    return all[item] === undefined ? (all[item] = 1 , all) : (all[item] += 1 , all);
     }, {});
 
     for (var numb in obj) {
       if (obj[numb] > modeCompare) {
-          modeCompare = obj[numb]
+          modeCompare = obj[numb];
       }
     }
 
-    for (var numb in obj) {
+    for (numb in obj) {
       if (obj[numb] === modeCompare) {
-        modeResult.push(parseInt(numb))
+        modeResult.push(parseInt(numb));
       }
     }
-    
+
   if (modeCompare === 1) {
-    return "no mode"
+    return "no mode";
   } else {
     return modeResult;
     }
   },
-}
+};
